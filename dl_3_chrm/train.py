a4a9167dc3afd19aefb8c27b444c645b273b0db7
Akira37 <863781751@qq.com>
2025-04-24 16:02:53 +0800

    Add DataLoader, EMA, progress bar, and plot_preds to train.py

--- dl_3_chrm/train.py
+++ dl_3_chrm/train.py
@@ -1,5 +1,56 @@
 from chrm_dataset import ChrmDataset
+from torch.utils.data import DataLoader
 from torchvision import transforms as tv
+import torch
+from util import draw_progress_bar, plot_preds
+
+class EMA():
+    def __init__(self, model, decay=0.98):
+        self.model = model
+        self.decay = decay
+        self.shadow = {}
+        self.backup = {}
+        self.register()
+        
+    def register(self):
+        """
+        初始化时调用，将student的参数初始化至shadow
+        """
+        for name, param in self.model.named_parameters():
+            if not param.requires_grad:
+                continue
+            self.shadow[name] = param.data.clone()
+        
+    def apply_shadow(self):
+        """
+        将student的参数备份到backup字典，然后将shadow的参数copy到student模型中
+        """
+        for name, param in self.model.named_parameters():
+            if param.requires_grad:
+                assert name in self.shadow
+                self.backup[name] = param.data
+                param.data = self.shadow[name]
+        
+    def restore(self):
+        """
+        将backup中的student参数copy回student模型中
+        """
+        for name, param in self.model.named_parameters():
+            if param.requires_grad:
+                assert name in self.backup
+                param.data = self.backup[name]
+        self.backup = {}
+            
+    def update(self):
+        """
+        将student的参数通过EMA更新到shadow中
+        """
+        for name, param in self.model.named_parameters():
+            if param.requires_grad:
+                assert name in self.shadow
+                new_average = (1.0 - self.decay) * param.data + self.decay * self.shadow[name]
+                self.shadow[name] = new_average.clone()
+        
 
 if __name__ == '__main__':
     transforms = tv.Compose([
@@ -11,6 +62,17 @@ if __name__ == '__main__':
       
     ])
     
-    dataset = ChrmDataset(data_roots={'/Users/hyperplasma/workspace/codes/Python/dl-test/dl_3_chrm/datasets/chrm_cls/with labels/train':1, 
+    train_dataset = ChrmDataset(data_roots={'/Users/hyperplasma/workspace/codes/Python/dl-test/dl_3_chrm/datasets/chrm_cls/with labels/train':1, 
                                       '/Users/hyperplasma/workspace/codes/Python/dl-test/dl_3_chrm/datasets/chrm_cls/without labels':0},
                           transforms=transforms)
+
+    batch_size = 12
+    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+    
+    test_dataset = ChrmDataset(data_roots={'/Users/hyperplasma/workspace/codes/Python/dl-test/dl_3_chrm/datasets/chrm_cls/with labels/test':1},
+                          transforms=transforms)
+
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=2)
+    
+    student = tv.models.resnet18(pretrained=True)
+    student.fc = torch.nn.Linear(student.fc.in_features, 2)

0454dd320b36c4936b9dcfd571225036a0e74d64
Akira37 <863781751@qq.com>
2025-05-15 16:08:25 +0800

    添加额外导入和数据加载功能，更新 SimCLR 模型以支持数据集训练

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # DL Course Practice
 
-loool
+Course 1: Flower102 CNN
+
+Course 2: 皮肤病分割
+
+Course 3: 染色体半监督分类
+
+Course 4: 染色体半监督分类 SimCLR
+
+ß
--- dl_4_ssl_chrm/SimCLR_train.py
+++ dl_4_ssl_chrm/SimCLR_train.py
@@ -2,12 +2,15 @@ import torch
 import torch.nn as nn
 from torchvision.models import resnet18
 import numpy as np
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from chrm_dataset import ChrmDataset
 
 class SimCLR(nn.Module):
     def __init__(self, base_model=resnet18, z_dim=128):
         super(SimCLR, self).__init__()
         # 定义encoder
-        self.encoder = base_model(pretrained=True)
+        self.encoder = base_model(pretrained=False)
         # 将fc层替换为projector
         self.encoder.fc = nn.Linear(self.encoder.fc.in_features, z_dim)
         
@@ -18,3 +21,21 @@ class SimCLR(nn.Module):
         z = self.encoder(x)
         
         return z, self.t.exp()
+
+
+transform = transforms.Compose([
+    transforms.RandomRotation(90, fill=225),
+    transforms.Resize((64, 64)),
+    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.ToTensor(),
+])
+
+train_dataset = ChrmDataset(data_roots={'datasets/chrm_cls/with labels/train':1, 
+                                      'datasets/chrm_cls/without labels':0},
+                           transforms=transforms)
+
+batch_size = 32
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+
+print('len(train_loader.dataset):', len(train_loader.dataset))
